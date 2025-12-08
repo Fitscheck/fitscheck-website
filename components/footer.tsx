022c291da9d514c5f5e1c58acc4969f4b54d8d5f
Obiekwe Agnes-Mary Chinaza <50022245+chinazaaa@users.noreply.github.com>
2025-12-08 21:22:29 +0000

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import { Instagram } from "lucide-react"
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-end md:justify-center bg-[#003366] text-gray-300 py-8 md:py-0 md:h-[401px]">
+    <footer className="flex items-center justify-end md:justify-center bg-[#003366] text-gray-300 py-8 md:py-0 md:h-[401px] mt-12 md:mt-20">
       <div className="flex flex-col items-center justify-center container gap-2 px-4 md:px-6">
         <h2 className="text-4xl md:text-[102.02px] font-bold text-[#F8E71C]" style={{ fontFamily: "var(--font-whyte-inktrap)" }}>
           FitsCheck
